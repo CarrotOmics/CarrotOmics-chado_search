1b26b563d40df241bafea895b1c72b8c6c668b40
dsenalik <dsenalik@wisc.edu>
2021-04-26 15:27:03 -0500

    Add links for analysis and JBrowse

--- chado_search/includes/search/carrotomics/carrotomics_marker_search.php
+++ chado_search/includes/search/carrotomics/carrotomics_marker_search.php
@@ -161,65 +161,6 @@ function chado_search_marker_search_base_query() {
 }
 
 /*************************************************************
- * JBrowse link builder
-*/
-// Determine if location can be viewed in JBrowse and if so
-// generate an appropriate url from location of e.g. "DCARv2_Chr5:8614051..8615973"
-// This requred modifications to api/result/Table.php to remove l() processing if
-// there is a question mark, otherwise the link is escaped and does not work
-function chado_search_marker_search_jbrowse_link ($location) {
-
-  // Some markers are on more than one map, so the same location may be found on
-  // multiple table lines. Create a local short-lived cache to avoid duplicate queries
-  static $jbrowselookup = [];
-
-  if ($location) {
-    // see if this location was cached from an earlier table line
-    if (array_key_exists($location, $jbrowselookup)) {
-      return $jbrowselookup[$location];
-    }
-    else {
-      # parse location, split on punctuation
-      $parts = preg_split("/[:\.]+/", $location);
-      if (count($parts) >= 3) {
-        $srcfeature = $parts[0];
-        $fmin = $parts[1];
-        $fmax = $parts[2];
-
-        // when displayed in JBrowse, add 100 b.p. margin on each side for better appearance
-        $margin = 100;
-        $fminmargin = $fmin>$margin?$fmin - $margin:1;
-        $fmaxmargin = $fmax + $margin;
-        $urlsuffix = $srcfeature . ':' . $fminmargin . '..' . $fmaxmargin;
-
-        // determine if present in JBrowse. The feature table has had the dbxref value
-        // set if present in JBrowse, see notebook page 78 July 11, 2020
-        $sql = "SELECT urlprefix from {db} WHERE db_id=(SELECT db_id FROM {dbxref}"
-          . " WHERE dbxref_id=(SELECT dbxref_id from {feature} WHERE type_id=(SELECT cvterm_id FROM {cvterm}"
-          . " WHERE name=:chromosome AND cv_id=(SELECT cv_id FROM {cv} WHERE name=:sequence)) AND uniquename=:uniquename))";
-        $results = chado_query($sql, [':chromosome' => 'chromosome', ':sequence' => 'sequence', ':uniquename' => $srcfeature]);
-        if ($results) {
-          $obj = $results->fetchObject();
-          if ($obj) {
-            // because the urlprefix from the db table is already encoded when Tripal
-            // JBrowse created it, we need to unencode it first, and add the leading /
-            // to make url not a relative url, and /data suffix as Tripal JBrowse uses
-            $jbrowseurl = '/' . urldecode($obj->urlprefix) . '/data&loc=' . $urlsuffix . '&highlight=' . $location;
-
-            // store in local cache and then return the url
-            $jbrowselookup[$location] = $jbrowseurl;
-            return $jbrowseurl;
-          }
-        }
-      }
-    }
-  }
-  // if anything fails, null is returned, which means no hyperlinks
-  // will be generated for the location
-  return NULL;
-}
-
-/*************************************************************
  * Build the search result table
 */
 // Define the result table
@@ -233,7 +174,7 @@ function chado_search_marker_search_table_definition () {
     'lg_uniquename:s' => 'Linkage Group',
     'start:s' => 'Start',
     'stop:s' => 'Stop',
-    'location:s:chado_search_marker_search_jbrowse_link:location' => 'Location'
+    'location:s:chado_search_jbrowse_link:location' => 'Location'
   ];
   return $headers;
 }
