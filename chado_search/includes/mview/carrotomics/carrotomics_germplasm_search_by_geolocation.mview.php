ef867ff80dc30920d48239699ccdf02e78229834
dsenalik <dsenalik@wisc.edu>
2021-11-12 11:40:46 -0600

    Add taxonomy filter to geolocation search

--- chado_search/includes/mview/carrotomics/carrotomics_germplasm_search_by_geolocation.mview.php
+++ chado_search/includes/mview/carrotomics/carrotomics_germplasm_search_by_geolocation.mview.php
@@ -23,6 +23,29 @@ function chado_search_create_germplasm_search_by_geolocation_mview() {
         'type' => 'varchar',
         'length' => '255'
       ),
+      'family' => array (
+        'type' => 'varchar',
+        'length' => 255,
+      ),
+      'genus' => array (
+        'type' => 'varchar',
+        'length' => 255,
+      ),
+      'species' => array (
+        'type' => 'varchar',
+        'length' => 255,
+        'not null' => true,
+      ),
+      'infraspecific_type' => array (
+        'type' => 'varchar',
+        'length' => 255,
+        'not null' => false,
+      ),
+      'infraspecific_name' => array (
+        'type' => 'varchar',
+        'length' => 255,
+        'not null' => false,
+      ),
       'stock_type' => array (
         'type' => 'varchar',
         'length' => '255'
@@ -65,6 +88,17 @@ function chado_search_create_germplasm_search_by_geolocation_mview() {
       (SELECT name FROM cvterm CVTT WHERE CVTT.cvterm_id = O.type_id
       AND CVTT.cv_id = (SELECT cv_id FROM cv WHERE name='taxonomic_rank')),
       'no_rank', ''), O.infraspecific_name) AS organism,
+    (SELECT OP.value FROM organismprop OP WHERE OP.organism_id = O.organism_id AND OP.type_id =
+      (SELECT F.cvterm_id FROM cvterm F WHERE F.name = 'superclass' AND F.cv_id =
+        (SELECT FF.cv_id FROM cv FF WHERE FF.name = 'taxonomic_rank'))) AS family,
+    O.genus,
+    O.species,
+    REPLACE (
+      (SELECT name FROM cvterm CVTT WHERE CVTT.cvterm_id = O.type_id
+        AND CVTT.cv_id = (SELECT cv_id FROM cv WHERE name='taxonomic_rank')),
+      'no_rank', '')
+      as infraspecific_type,
+    O.infraspecific_name,
     V.name AS stock_type,
     NDG.latitude, NDG.latitude_dev, NDG.longitude, NDG.longitude_dev, NDG.altitude, NDG.altitude_dev,
     GEO.country,
--- chado_search/includes/search/carrotomics/carrotomics_germplasm_search_by_geolocation.php
+++ chado_search/includes/search/carrotomics/carrotomics_germplasm_search_by_geolocation.php
@@ -47,6 +47,43 @@ function chado_search_germplasm_search_by_geolocation_form ($form) {
       ->newLine()
   );
 
+  $form->addSelectFilter(
+      Set::selectFilter()
+      ->id('family')
+      ->title('Familiy')
+      ->column('family')
+      ->table('chado_search_species')
+  );
+  $form->addDynamicSelectFilter(
+      Set::dynamicSelectFilter()
+      ->id('genus')
+      ->title('Genus')
+      ->dependOnId('family')
+      ->callback('chado_search_geolocation_ajax_genus')
+  );
+  $form->addDynamicSelectFilter(
+      Set::dynamicSelectFilter()
+      ->id('species')
+      ->title('Species')
+      ->dependOnId('genus')
+      ->callback('chado_search_geolocation_ajax_species')
+  );
+  $form->addDynamicSelectFilter(
+      Set::dynamicSelectFilter()
+      ->id('infraspecific_type')
+      ->title('Infraspecific type')
+      ->dependOnId('species')
+      ->callback('chado_search_geolocation_ajax_infraspecific_type')
+  );
+  $form->addDynamicSelectFilter(
+      Set::dynamicSelectFilter()
+      ->id('infraspecific_name')
+      ->title('Infraspecific name')
+      ->dependOnId('infraspecific_type')
+      ->callback('chado_search_geolocation_ajax_infraspecific_name')
+      ->newLine()
+  );
+
   $form->addSubmit();
   $form->addReset();
   $form->addFieldset(
@@ -97,35 +134,51 @@ function chado_search_germplasm_search_by_geolocation_form_validate ($form, &$fo
     form_set_error('', t('A positive distance value in kilomenters is required.'));
   }
   if (!(preg_match('/^[\d\.]+$/', $querydistance))) {
-    form_set_error('', t('The distance as entered is not valid, it must be a positive number.'));
+    form_set_error('', t('The distance as entered is not valid, it must be a positive number without "+".'));
+  }
+  if (preg_match('/\..*\./', $querydistance)) {
+    form_set_error('', t('The distance as entered is not valid, it has more than one period character.'));
   }
 }
 
 // Submit the form
 function chado_search_germplasm_search_by_geolocation_form_submit ($form, &$form_state) {
 
-  // three values from the form are needed to construct the distance filter
+  // Here the base SQL is rather involved, as we need to calculate distance
+  // between two points on the surface of the earth. We use the Haversine
+  // formula in a sub query to generate a distance value that we can then
+  // use in a WHERE clause. This is then itself embedded in another sub query
+  // so that we can use the usual chado query tools for the other taxonomy filters.
+  // Three values from the form are needed to construct the distance filter.
+  // geoconvert() and form validation will sanitize the user input that is
+  // embedded in this SQL.
   $querylat = geoconvert( $form_state['values']['latitude'] );
   $querylong = geoconvert( $form_state['values']['longitude'] );
   $querydistance = $form_state['values']['distance'];
 
-  // The formula in the subquery calculates the great-circle distance between two
-  // points using the Haversine formula, distance units are km
-  $sql = "SELECT * FROM (SELECT *,"
-       . " ROUND( CAST( 6378.137 * acos( cos( radians($querylat) ) * cos( radians( latitude ) )"
-       . " * cos( radians( longitude ) - radians($querylong) ) + sin( radians($querylat) )"
-       . " * sin( radians(latitude))) AS NUMERIC), 1)"
-       . " AS distance from {chado_search_germplasm_search_by_geolocation}) CALC"
-       . " WHERE latitude IS NOT NULL AND longitude IS NOT NULL"
-       . " AND distance <= $querydistance"
-       . " ORDER BY distance";
+  // The constant 6378.137 is the earth's radius in km
+  // The distance calculation is rounded to the nearest 0.1 km
+  $sql = "SELECT * FROM (SELECT * FROM (SELECT *,"
+       . " ROUND( CAST( 6378.137 * acos( cos( radians($querylat) ) * cos( radians(latitude) )"
+       . " * cos( radians(longitude) - radians($querylong) ) + sin( radians($querylat) )"
+       . " * sin( radians(latitude))) AS NUMERIC), 1) AS distance"
+       . " FROM {chado_search_germplasm_search_by_geolocation}) SUB2"
+       . " WHERE latitude IS NOT NULL"
+       . " AND longitude IS NOT NULL"
+       . " AND distance <= $querydistance) SUB1";
 
-  // Add conditions, none added here because we are using Haversine filter above
+  // Filter by taxonomy values if selected
   $where = [];
+  $where[] = Sql::selectFilter('family', $form_state, 'family');
+  $where[] = Sql::selectFilter('genus', $form_state, 'genus');
+  $where[] = Sql::selectFilter('species', $form_state, 'species');
+  $where[] = Sql::selectFilter('infraspecific_type', $form_state, 'infraspecific_type');
+  $where[] = Sql::selectFilter('infraspecific_name', $form_state, 'infraspecific_name');
 
   Set::result()
     ->sql($sql)
     ->where($where)
+    ->defaultOrder('distance')
     ->tableDefinitionCallback('chado_search_germplasm_search_by_geolocation_table_definition')
     ->execute($form, $form_state);
 }
@@ -154,7 +207,8 @@ function chado_search_germplasm_search_by_geolocation_table_definition () {
 /**
  * Converts an entered latitude or longitude into decimal form
  * from either degrees and decimal minutes, or degree minute second
- * notation, to decimal degree format
+ * notation, to decimal degree format.
+ *
  * @param float $value 
  * @return float decimalized version or null string if error
  */
@@ -180,5 +234,29 @@ function geoconvert ( $value ) {
   else {
     $value = '';
   }
+  // final check for more than one decimal place
+  if (preg_match('/\..*\./', $value)) {
+    $value = '';
+  }
   return($value);
+}
+
+/*************************************************************
+ * AJAX callbacks
+ */
+function chado_search_geolocation_ajax_genus ($val) {
+  $sql = "SELECT distinct genus FROM {chado_search_species} WHERE family = :family ORDER BY genus";
+  return chado_search_bind_dynamic_select(array(':family' => $val), 'genus', $sql);
+}
+function chado_search_geolocation_ajax_species ($val) {
+  $sql = "SELECT distinct species FROM {chado_search_species} WHERE genus = :genus ORDER BY species";
+  return chado_search_bind_dynamic_select(array(':genus' => $val), 'species', $sql);
+}
+function chado_search_geolocation_ajax_infraspecific_type ($val) {
+  $sql = "SELECT distinct infraspecific_type FROM {chado_search_species} WHERE species = :species ORDER BY infraspecific_type";
+  return chado_search_bind_dynamic_select(array(':species' => $val), 'infraspecific_type', $sql);
+}
+function chado_search_geolocation_ajax_infraspecific_name ($val) {
+  $sql = "SELECT distinct infraspecific_name FROM {chado_search_species} WHERE infraspecific_type=:infraspecific_type ORDER BY infraspecific_name";
+  return chado_search_bind_dynamic_select(array(':infraspecific_type' => $val), 'infraspecific_name', $sql);
 }
